982ea871730eab4b56eb6a9628292abc5f957dd4
prathap <prathap.mohan@kloctechnologies.co.in>
2019-01-06 15:34:48 +0530

    made some basic changes

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//REST - Representational State Transfer
+
+
 app.use((req,res,next)=>{
     console.log(`${req.url}--${req.method}--${req.ip}--${new Date()}`);
     next();
@@ -16,3 +19,6 @@ app.listen(port,()=>{
   console.log("listening on the port 3000");
 })
 
+app.get('/products',(req,res)=>{
+  res.send('{msg: "this is the products catalog"}');
+})
