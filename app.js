6544b6ef899edcb4f65c164b072a489b8bb50523
prathap <prathap.mohan@kloctechnologies.co.in>
2019-01-06 16:26:09 +0530

    as of now we have played with the data and now from here we use body parser to have access to the body object and to see the content in it we insert the body parser package

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = express();
 const port = 3000;
 
 //REST - Representational State Transfer
+//let us understand the rest end point with the products
+//GET - /products this is used for gettig all the products (thiss comes under the read of the CRUD)
+
+//POST - /products this is 
+
 
 
 app.use((req,res,next)=>{
@@ -19,6 +24,75 @@ app.listen(port,()=>{
   console.log("listening on the port 3000");
 })
 
+
+
+//Now we are doing the rest  operations here and one more method we have here is 
+//req.params.id this basically gives the id out of the url but the thing that we need to do is 
+//pass the url in such a way that /products/:id format
+
+//Now as we dont have any data base that is where we try to take the products into the array later find the products that which we require 
+var products = [
+    {
+        "id": 1,
+        "name": "pro1",
+        "description": "this is description for the product 1"
+    },
+    {
+        "id": 2,
+        "name": "pro2",
+        "description": "this is the description for the product 2"
+    },
+    {
+        "id": 3,
+        "name": "pro3",
+        "description": "this is the description for the product 3"
+    },
+    {
+        "id": 4,
+        "name": "pro4",
+        "descripiton": "this isi the description for the product 4"
+    }
+]
+
 app.get('/products',(req,res)=>{
-  res.send('{msg: "this is the products catalog"}');
+  if(products.length > 0){
+      res.send({"msg": products})
+  }
+  else{
+    res.send({"msg": "there exists no products"})
+  }
+})
+
+app.post('/products',(req,res)=>{
+  console.log(req);  
+  res.send('{msg: "attempted to insert the record of product"}');
+  //Now here we try to create a record of the product and try to insert  into the database but as we dont have any data base here we try to push the product into the array
+})
+
+app.put('/products/:id',(req,res)=>{
+    let found_product = products.find((product)=>{
+        return product.id == req.params.id
+    })
+    if(found_product){
+      res.send({msg: "attempted to update the producct with id "+req.params.id})
+    }
+    else{
+      res.send({msg: "attempted to accesss a record with the id that doesnot exist"})
+    }  
+})
+
+app.delete('/products/:id',(req,res)=>{
+    res.send({"msg": `attempted to destroy the product with id ${req.params.id}`})
+})
+
+app.get('/products/:id',(req,res)=>{
+    let found_product = products.find((product)=>{
+        return product.id == req.params.id
+    });
+    if(found_product){
+      res.send({msg: "Hi bro! attempted to read the producct with id "+req.params.id})
+    }
+    else{
+    res.send({msg: "attempted to accesss a record with the id that doesnot exist"})
+    }
 })
